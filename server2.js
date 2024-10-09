6c38592b73f954654439d723dc6a11f271a017c1
Rushs321 <fyggh566@tutamail.com>
2024-10-09 06:15:09 +0000

    update

--- server2.js
+++ server2.js
@@ -4,10 +4,10 @@
 
 const app = require('express')()
 const proxy = require('./src/proxy1')
-
+const params = require('./src/params')
 const PORT = process.env.PORT || 8080
 
 
-app.get('/', proxy)
+app.get('/', params, proxy)
 app.get('/favicon.ico', (req, res) => res.status(204).end())
 app.listen(PORT, () => console.log(`Worker ${process.pid}: Listening on ${PORT}`))
