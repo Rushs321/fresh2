5d522f28bc5099b52c6171b1601e258434776b4d
Rushs321 <fyggh566@tutamail.com>
2024-10-09 10:45:46 +0530

    Create server2.js

---
+++ server2.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+'use strict'
+
+
+const app = require('express')()
+const proxy = require('./src/proxy')
+
+const PORT = process.env.PORT || 8080
+
+
+app.get('/', proxy)
+app.get('/favicon.ico', (req, res) => res.status(204).end())
+app.listen(PORT, () => console.log(`Worker ${process.pid}: Listening on ${PORT}`))
